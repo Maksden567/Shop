b19ecfc7fd7d9e93040b50d1223bea44580a6642
Denmaks234 <denmaks432@gmail.com>
2022-08-12 11:24:53 +0300

    1440 header one project

--- Desktop/gulp-2022/src/js/app.js
+++ Desktop/gulp-2022/src/js/app.js
@@ -39,13 +39,38 @@ closeBtn.addEventListener('click',function(){
     email.style.top="505px"
     window.location.pathname=="/contact.html"? headerText.style.display="none":headerText.style.display="block"
      screen.width<=425 ? headerInfo.style.marginTop="110px":headerInfo.style.marginTop="210px"
+    
      burgerBtn.style.display="block"
      screen.width<=425 ? menuTitle.style.display="none":menuTitle.style.display="block"
      screen.width<=850 ?  header.style.height="959px": null
      screen.width<=425 ?  header.style.height="542px":null
+     window.location.pathname=="/news.html"?header.style.height="160px":null
+     window.location.pathname=="/news.html"  && screen.width<=850?header.style.height="136px":null
+     window.location.pathname=="/news.html"  && screen.width<=420?header.style.height="116px":null
+     window.location.pathname=="/news.html"?headerBottom.style.display="none":null
+     window.location.pathname=="/news.html"?headerText.style.display="none":null
+     window.location.pathname=="/about.html"?header.style.height="160px":null
+     window.location.pathname=="/about.html"  && screen.width<=1100?header.style.height="96px":null
+     window.location.pathname=="/about.html"  && screen.width<=1100?header.style.background="white":null
+     window.location.pathname=="/about.html"?headerBottom.style.display="none":null
+     window.location.pathname=="/about.html"?headerText.style.display="none":null
+     window.location.pathname=="/projects.html"?header.style.height="160px":null
+     window.location.pathname=="/projects.html"  && screen.width<=1100?header.style.height="96px":null
+     window.location.pathname=="/projects.html"  && screen.width<=1100?header.style.background="white":null
+     window.location.pathname=="/projects.html"?headerBottom.style.display="none":null
+     window.location.pathname=="/projects.html"?headerText.style.display="none":null
+     window.location.pathname=="/one.html"?header.style.height="162px":null
+     window.location.pathname=="/one.html"  && screen.width<=850?header.style.height="138px":null
+     window.location.pathname=="/one.html"  && screen.width<=420?header.style.height="118px":null
+     window.location.pathname=="/one.html"  && screen.width<=1100?header.style.background="white":null
+     window.location.pathname=="/one.html"?headerBottom.style.display="none":null
+     window.location.pathname=="/one.html"?headerText.style.display="none":null
  })
 
 
+
+ window.location.pathname=="/news.html"  && screen.width<=1024 ? footer.style.background="url(../img/bg__1024.png)":null
+ window.location.pathname=="/news.html"  && screen.width<=850 ? footer.style.background="url(../img/bg__768.png)":null
 
 
 
