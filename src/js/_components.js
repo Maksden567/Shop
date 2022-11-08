e5030de37dd2790b9a3d544a767b36bb39e0e3eb
Maksden567 <babadid346@gmail.com>
2022-11-08 23:02:53 +0200

    title

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,6 @@
-console.log('components');
+let subtitle = document.querySelector(".main__subtitle>p")
+let title = document.querySelector(".swiper-slide>img")
+if(screen.width<550){
+    title.setAttribute('src',"../img/title-bg380.png")
+    subtitle.textContent="We are an online plant shop offering a wide range"
+}
